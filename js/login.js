6acdfccadc05a4762cdf74f177997f12a8661b90
MarianoCarrizo <warlockphantasy@gmail.com>
2022-11-27 21:51:38 -0300

    login fixed!

--- js/login.js
+++ js/login.js
@@ -1,28 +1,34 @@
-const urlLogin = 'https://localhost:7297/api/admin/login';
+const urlLogin = 'https://localhost:7298/api/admin/login';
 const buttonLogin = document.getElementById('login');
 
-buttonLogin.addEventListener('click', ()=> {
+buttonLogin.addEventListener('click', (callback)=> {
     let jsonBody = {
         email : document.getElementById("email").value,
         password : document.getElementById("password").value,
     };
     (async () => {
-        const rawResponse = await fetch(urlLogin, {
+        const rawResponse = await fetch('https://localhost:7298/api/admin/login', {
           method: 'POST',
           headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify(jsonBody)
-        });
+          body: JSON.stringify(jsonBody),
+          
+        })
         const content = await rawResponse.json();
-        console.log(content.status)
-        if(content.status === "exito"){
-            LoginHandler(content,callback)
-            window.location.href="index.html"
-        }else{
-            window.location.href="register.html"
+        console.log(content);
+        if(content.message === "exito"){
+          LoginHandler(content,callback)
+          window.location.href="index.html"
+      }else{
+          window.location.href="register.html"
         }
       })();
 })
+
+async function LoginHandler(body,callback){
+  var json = await body;
+  callback(json)
+}
 
